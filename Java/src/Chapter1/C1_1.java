083252e670f3da81992d0df1f910b620e8624dd2
gaiat <gaiat@192.168.40.34>
2023-12-20 14:44:35 +0900

    "finish 1-1"

---
+++ Java/src/Chapter1/C1_1.java
@@ -0,0 +1,12 @@
+package Chapter1;
+
+// 1.2 Javaプログラムの基本構造
+
+public class C1_1 {
+	public static void main(String[] args)	{
+		System.out.println("RPG:スッキリ魔王討伐");
+		System.out.println("Ver.0.1 by Y");
+		System.out.println("<ただいま鋭意学習・制作中>");
+		System.out.println("プログラムを終了します");
+	}
+}
