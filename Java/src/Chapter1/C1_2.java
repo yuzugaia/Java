6b412b0a9ea9d964dbf2c3da44fd66737b3476ee
gaiat <gaiat@192.168.40.34>
2023-12-20 16:37:43 +0900

    "finsh1-2"

---
+++ Java/src/Chapter1/C1_2.java
@@ -0,0 +1,11 @@
+package Chapter1;
+
+// 1.3 変数宣言の文
+
+public class C1_2 {
+	public static void main(String[] args) {
+		int age;					//変数の宣言
+		age = 30;					//変数に代入
+		System.out.println(age);	//代入された値の表示
+	}
+}
