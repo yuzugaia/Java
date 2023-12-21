317196700a9b0a29b76a94fe15ef4ebff81c5dfe
gaiat <gaiat@192.168.40.34>
2023-12-21 12:36:20 +0900

    "finish 2-1"

---
+++ Java/src/Chapter2/C2_1.java
@@ -0,0 +1,14 @@
+package Chapter2;
+
+// 2.1.1 計算式の文とは？
+
+public class C2_1 {
+	public static void main(String[] args) {
+		int a;	//変数宣言
+		int b;
+		a = 20;	//計算(代入)
+		b = a + 5;	//計算(足し算して代入)
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
