4b3a09fc261e4df2bcbf5c2fc82987ccbd0b5e0e
yuzugaia <yuzuki3701@gmail.com>
2023-12-22 18:37:21 +0900

    "finish2-8,2-9"

---
+++ Java/src/Chapter2/C2_8.java
@@ -0,0 +1,12 @@
+package Chapter2;
+
+// 2.5.4 演算時の自動型変換 2-8 異なる型同士の算術演算
+
+public class C2_8 {
+	public static void main(String[] args) {
+		double d = 8.5 / 2;
+		long l = 5 + 2L;
+		System.out.println(d);
+		System.out.println(l);
+	}
+}
---
+++ Java/src/Chapter2/C2_9.java
@@ -0,0 +1,10 @@
+package Chapter2;
+
+// 2.5.4 演算時の自動型変換 2-9 文字列の連結
+
+public class C2_9 {
+	public static void main(String[] args) {
+		String msg = "私の年齢は" + 23;
+		System.out.println(msg);
+	}
+}
