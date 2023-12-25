f349a0b5766897199e226c37dc293f6f84cbcf6d
yuzugaia <yuzuki3701@gmail.com>
2023-12-25 14:09:45 +0900

    "finish2-10"

---
+++ Java/src/Chapter2/C2_10.java
@@ -0,0 +1,12 @@
+package Chapter2;
+
+// 2.6.1 命令の実行の文とは 2-10 命令実行の文(画面に表示する)
+
+public class C2_10 {
+	public static void main(String[] args) {
+		String name = "すがわら";
+		String message;
+		message = name + "さん、こんにちは";
+		System.out.println(message);
+	}
+}
