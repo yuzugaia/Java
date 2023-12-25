32e4e341affd258740306b3c8e4c51ee1e6b4ed8
yuzugaia <yuzuki3701@gmail.com>
2023-12-25 18:01:04 +0900

    "finish2-15"

--- Java/src/Chapter2/C2_15.java
+++ Java/src/Chapter2/C2_15.java
@@ -4,10 +4,10 @@ package Chapter2;
 
 public class C2_15{
 	public static void main(String[] args) {
-		System.out.println("あなたの名前を入力してください。");
+		System.out.println("あなたの名前を入力してください。");		//1行の文字列の入力を受け付ける
 		String name = new java.util.Scanner(System.in).nextLine();
 		System.out.println("あなたの年齢を入力してください。");
-		int age = new java.util.Scanner(System.in).nextInt();
+		int age = new java.util.Scanner(System.in).nextInt();		//1行の整数の入力を受け付ける
 		System.out.println("ようこそ、" + age + "歳の" + name + "さん" );
 	}
 }
