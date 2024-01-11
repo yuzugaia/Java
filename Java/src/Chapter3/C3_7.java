70292e36e5825f07a8c75d7fc3e271c7ca9ecc9c
yuzugaia <yuzuki3701@gmail.com>
2024-01-11 22:29:34 +0900

    "finish3-7"

---
+++ Java/src/Chapter3/C3_7.java
@@ -0,0 +1,11 @@
+package Chapter3;
+
+//3.5.2 for文による繰り返し 3-7基本的なfor文
+
+public class C3_7{
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("こんにちは");
+		}
+	}
+}
