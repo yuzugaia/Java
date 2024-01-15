7f5fa05e9ac750a85109fd2cbd5af14ced2e4398
yuzugaia <yuzuki3701@gmail.com>
2024-01-15 17:12:36 +0900

    "finish3-8"

---
+++ Java/src/Chapter3/C3_8.java
@@ -0,0 +1,11 @@
+package Chapter3;
+
+//3.5.4 ループ変数 3-8 for文のループ変数iの内容を表示する
+
+public class C3_8{
+	public static void main(String[] args) {
+		for (int i = 0; i < 3; i++) {
+			System.out.print("現在" + (i +1) + "周目→");
+		}
+	}
+}
