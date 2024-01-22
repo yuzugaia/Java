ead6184b9cf496fa9c77724f298f08bd0087083f
yuzugaia <yuzuki3701@gmail.com>
2024-01-22 14:46:37 +0900

    "finish4-3,4-4"

---
+++ Java/src/Chapter4/C4_3.java
@@ -0,0 +1,9 @@
+package Chapter4;
+
+//4.2.1 配列の作成 4-3 配列の作成手順(配列変数の宣言と要素の作成、代入を同時に行う)
+
+public class C4_3 {
+	public static void main(String[] args) {
+		int[] scores = new int[5];
+	}
+}
---
+++ Java/src/Chapter4/C4_4.java
@@ -0,0 +1,11 @@
+package Chapter4;
+
+//4.2.1 配列の作成 4-3 配列のの長さを調べる
+
+public class C4_4 {
+	public static void main(String[] args) {
+		int[] scores = new int[5];
+		int num = scores.length;
+		System.out.println("要素の数：" + num);
+	}
+}
