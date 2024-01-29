6bda1d66e15fe67edb150ec83ce048ede08e1759
yuzugaia <yuzuki3701@gmail.com>
2024-01-29 16:54:53 +0900

    "finish4-11"

---
+++ Java/src/Chapter4/C4_11.java
@@ -0,0 +1,16 @@
+package Chapter4;
+
+//4.4.3パターン2 4-11 50点以上の科目の数を調べる
+
+public class C4_11{
+	public static void main(String args[]) {
+		int[] scores = {20, 30, 40, 50, 80};
+		int count = 0;
+		for (int i = 0; i < scores.length; i++) {
+			if (scores[i] >= 50) {
+				count++;
+			}
+		}
+		System.out.println("50点以上の科目の数は:" + count);
+	}
+}
