f1d828e025ca0feb28acca858da13185d41ee13a
yuzugaia <yuzuki3701@gmail.com>
2024-02-01 19:49:29 +0900

    "finish4-13"

---
+++ Java/src/Chapter4/C4_13.java
@@ -0,0 +1,12 @@
+package Chapter4;
+
+//4.4.5拡張for文 4-13従来のfor文
+
+public class C4_13{
+	public static void main(String[] args) {
+		int[] scores = {20, 30, 40, 50, 80};
+		for (int i = 0; i < scores.length; i++) {
+			System.out.println(scores[i]);
+		}
+	}
+}
