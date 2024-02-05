5a142c5eef3f19835cad0c0ca5969b5da1d1f5da
yuzugaia <yuzuki3701@gmail.com>
2024-02-05 19:52:02 +0900

    "finish4-16"

---
+++ Java/src/Chapter4/C4_16.java
@@ -0,0 +1,12 @@
+package Chapter4;
+
+//4.6.1ガベージコレクション 4-16ガベージコレクション
+
+public class C4_16{
+	public static void main(String[] args) {
+		boolean judge = true;
+		if (judge == true) {
+			int[] array = {1, 2, 3};
+		}
+	}
+}
