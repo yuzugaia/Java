a637d8201bd09989ce8b6b1d15ac75809ea51fad
yuzugaia <yuzuki3701@gmail.com>
2024-02-11 18:15:25 +0900

    "finish5-1"

---
+++ Java/src/Chapter5/C5_1.java
@@ -0,0 +1,8 @@
+package Chapter5;
+//5.1.2メソッドの定義 5-1シンプルなメソッドの定義
+
+public class C5_1{
+	public static void hello() {	//helloメソッドの定義
+		System.out.println("湊さん、こんにちは");	//helloメソッドが呼び出された時の処理
+	}
+}
