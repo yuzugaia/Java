919494755f0e5b0c09cc74724da9d9f9ef16e5e8
yuzugaia <yuzuki3701@gmail.com>
2024-02-27 14:54:31 +0900

    "finish5-8"

---
+++ Java/src/Chapter5/C5_8.java
@@ -0,0 +1,11 @@
+package Chapter5;
+//5.3.3戻り値をそのまま使う 5-8戻り値をそのまま使う
+public class C5_8{
+	public static int add(int x, int y) {
+		int ans = x + y;
+		return ans;
+	}
+	public static void main(String[] args) {
+		System.out.println(add(add(10, 20), add(30, 40)));
+	}
+}
